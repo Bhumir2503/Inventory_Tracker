2b955be4869080bc58bfaa164b0f27c5cbe5b72c
Bhumir Patel <bhumir2503@gmail.com>
2024-03-06 22:29:58 -0500

    Routes Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,31 @@
-import './App.css'
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import PageNotFound from './pages/pageNotFound'
+import Login from './pages/auth/login'
+import Register from './pages/auth/register'
+import Dashboard from './pages/user/dashboard'
+import Contacts from './pages/user/contacts'
+import Inventory from './pages/user/inventory'
+import Purchases from './pages/user/purchases'
+import Sales from './pages/user/sales'
+
+
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/*" element={<h1>Testing</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contact" element={<Contacts />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/purchase" element={<Purchases />} />
+        <Route path="/sales" element={<Sales />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   )
