9ed69ef7eab4a4e80528607c5514a7833d5f538d
Bhumir Patel <bhumir2503@gmail.com>
2024-03-06 23:25:12 -0500

    home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import '../styles/home.css';
+import check from '../assets/check.svg';
+import angle from '../assets/angle-right.svg';
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='home'>
+      <div className="left">
+        <h1>Inventory Tracking Made Easy.</h1>
+        <h2>Manage your inventory with ease. Keep track of your products, sales, and more.</h2>
+        <div className="list">
+          <img src={check} alt="" />
+          <p>Consolidate and streamline your inventory management process.</p>
+        </div>
+        
+        <div className="list">
+          <img src={check} alt="" />
+          <p>Track inventory, contacts, sales, and more.</p>
+        </div>
+
+        <div className="list">
+          <img src={check} alt="" />
+          <p>Perfect for small business inventory management.</p>
+        </div>
+        
+        <div className="actionbutton">
+          <a href="/register" className="button">Get Started</a>
+          <a href="/login" className='link'>Login <img src={angle} alt="" /></a>
+        </div>
+      </div>
+      <div className="right">
+        <h1>Img goes Here</h1>
+      </div>
     </div>
   );
 }
