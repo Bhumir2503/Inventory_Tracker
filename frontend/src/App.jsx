9dc035408862bd27d1cf4e5d44ff9e366e514683
Bhumir Patel <bhumir2503@gmail.com>
2024-03-06 22:02:43 -0500

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
 import './App.css'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/about" element={<h1>About</h1>} />
         <Route path="/*" element={<h1>Testing</h1>} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
