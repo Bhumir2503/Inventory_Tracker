4b50e95b77b9fb6f7a73425867341117441fa5d0
Bhumir Patel <bhumir2503@gmail.com>
2024-03-07 16:33:56 -0500

    Login and Register Pages and CSS

--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -1,9 +1,22 @@
-
+import React from 'react';
+import '../../styles/login.css'
+import logo from '../../assets/logo.svg';
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='login'>
+      <form action="">
+        <img src={logo} alt="" />
+        <h2>Sign in to your account</h2>
+        <div className="inputs">
+          <label className='header'>Username</label>
+          <input type="text" placeholder="Username" />
+          <label className='header'>Password</label>
+          <input type="password" placeholder="Password" />
+          <button type="submit">Sign In</button>
+          <label className='NotMember'>Not a member? <a href="/register">Get Started!</a></label>
+        </div>
+      </form>
     </div>
   );
 }
--- frontend/src/pages/auth/register.jsx
+++ frontend/src/pages/auth/register.jsx
@@ -1,11 +1,26 @@
-
+import React from 'react';
+import '../../styles/register.css'
+import logo from '../../assets/logo.svg';
 
 function Register () {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='register'>
+      <form action="">
+        <img src={logo} alt="" />
+        <h2>Sign up for an account</h2>
+        <div className="inputs">
+          <label className="header">Email</label>
+          <input type="email" placeholder="Email" />
+          <label className='header'>Username</label>
+          <input type="text" placeholder="Username" />
+          <label className='header'>Password</label>
+          <input type="password" placeholder="Password" />
+          <button type="submit">Sign Up</button>
+          <label className='Member'>Already a member? <a href="/login">Sign In!</a></label>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
 export default Register
